0b75dd70586df60b4db4b28462204c8efa234a7a
unknown <brisova.majka@gmail.com>
2016-06-08 23:58:03 +0200

    setup

--- setup.py
+++ setup.py
@@ -3,19 +3,12 @@
 from setuptools import setup
 
 setup(
-    # GETTING-STARTED: set your app name:
-    name='YourAppName',
-    # GETTING-STARTED: set your app version:
+    name='beMyGuest',
     version='1.0',
-    # GETTING-STARTED: set your app description:
-    description='OpenShift App',
-    # GETTING-STARTED: set author name (your name):
-    author='Your Name',
-    # GETTING-STARTED: set author email (your email):
-    author_email='example@example.com',
-    # GETTING-STARTED: set author url (your url):
-    url='http://www.python.org/sigs/distutils-sig/',
-    # GETTING-STARTED: define required django version:
+    description='Lightweight hotel room scheduler',
+    author='briska',
+    author_email='brisova.majka@gmail.com',
+    url='https://github.com/briska/',
     install_requires=[
         'Django==1.8.4'
     ],
