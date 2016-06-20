afcd0a844bf6b0da7c57cec09755540c3c9e4076
unknown <brisova.majka@gmail.com>
2016-06-20 10:07:09 +0200

    staticfiles dirs for collectstatic

--- wsgi/bemyguest/bemyguest/settings.py
+++ wsgi/bemyguest/bemyguest/settings.py
@@ -123,7 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(WSGI_DIR, 'static')
 
-if os.environ.get('LOCAL', False):
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
